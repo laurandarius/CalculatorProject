cf8178ae6f047467e8e0079d6b1f86b78aabbb19
Darius Lauran <darius.lauran@algotech.solutions>
2019-04-19 12:48:41 +0300

    Add Button component.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,9 @@
 import React, { useState } from "react";
 
-const Button = props => {
-  const [value, setValue] = useState(props.value);
-  return (
-    <div>
-      <button>{value}</button>
-    </div>
-  );
-};
+const Button = props => (
+  <div>
+    <button>{props.value}</button>
+  </div>
+);
 
 export default Button;
