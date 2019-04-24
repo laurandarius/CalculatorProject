5604059bc5c5db9556bdfa7e9c29bc183a582c57
Darius Lauran <darius.lauran@algotech.solutions>
2019-04-24 10:34:52 +0300

    Add parameterised style properties to the Button component.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,27 @@
+import React from "react";
 import styled from "styled-components";
 
+const sizes = {
+  small: "58.2px",
+  large: "116.6px"
+};
+
 const ButtonLayout = styled.button`
-  width: 58.2px;
+  width: ${props => props.size};
   height: 47.5px;
-  background-color: gainsboro;
+  background-color: ${props => props.backgroundColor};
   font-size: 22px;
-  color: black;
+  color: ${props => props.color};
 `;
 
-const Button = props => <ButtonLayout>{props.value}</ButtonLayout>;
+const Button = props => (
+  <ButtonLayout
+    backgroundColor={props.backgroundColor}
+    size={sizes[props.size ? "large" : "small"]}
+    color={props.color}
+  >
+    {props.children}
+  </ButtonLayout>
+);
 
 export default Button;
